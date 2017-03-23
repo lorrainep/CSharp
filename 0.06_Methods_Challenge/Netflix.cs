b6a67894e75864468943c462646950ff88854e7f
Lorraine Papczynski <Lorraine Papczynski>
2017-03-23 11:07:48 -0400

    more stuff

---
+++ 0.06_Methods_Challenge/Netflix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._06_Methods_Challenge
+{
+    class Netflix
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; }
+        public int TotalMinutes { get; set; }
+
+        //Public Void
+        public void GetNetflix()
+        {
+            Console.WriteLine("I have watched {0}", Name);
+        }
+        public int ShowRuntime()
+        {
+            Console.WriteLine(this.TotalMinutes);
+            return this.TotalMinutes;
+        }
+    }
+
+}
---
+++ 0.06_Methods_Challenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._06_Methods_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Netflix movieOne = new Netflix();
+            movieOne.Name = "Movie One";
+            movieOne.Genre = "Comedy";
+            movieOne.Rating = 4;
+            movieOne.GetNetflix();
+
+            if (movieOne.Rating >= 4)
+            {
+                Console.WriteLine("This is an OKAY movie");
+            }
+            else
+            {
+                Console.WriteLine("This movie SUCKED");
+            }
+            Console.ReadLine();
+        }
+    }
+}
